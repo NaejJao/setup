0ed15195e89731fd20327482187a515337335138
Jean Schifflers <j.schifflers@gmail.com>
2025-09-25 11:56:55 +0200

    Update lix_setup.sh
    source bashrc after installation to be able to use it directly.

--- Nix/lix_setup.sh
+++ Nix/lix_setup.sh
@@ -1,6 +1,9 @@
 #install Lix ( like Nix )
 curl -sSf -L https://install.lix.systems/lix | sh -s -- install
 
+#to be able to use the updated PATH
+source ~/.bashrc
+
 # install base packages
 nix profile install nixpkgs#fastfetch
 nix profile install nixpkgs#mc
